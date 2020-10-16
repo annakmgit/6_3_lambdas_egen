eafe6d552d96a234cc34f4650e0633eaeb4aaf0a
annakmgit <annakm@mac.com>
2020-10-16 16:16:58 +0300

    Removed all substance from Predicate

--- src/main/java/Predicate.java
+++ src/main/java/Predicate.java
@@ -1,14 +1,16 @@
 
 @FunctionalInterface
-public interface Predicate {
+public interface Predicate{
 
-    default boolean test(Measurable m){
 
-        if (m.getValue() > 5) return true;
-        return false;
-    }
 }
 
 //This method is supposed to return something but it need one, and only one,
 // abstract method to be a functional interface.
 
+
+    /*default boolean test(Measurable m){
+
+        if (m.getValue() > 5) return true;
+        return false;
+    }*/
