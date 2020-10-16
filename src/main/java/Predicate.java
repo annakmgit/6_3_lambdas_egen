98fd047e10adfcae5d307e44e7ae2e6a67e0a81a
ngrundback <ngrundback@gmail.com>
2020-10-16 15:24:12 +0200

    Revert Predicate.java

--- src/main/java/Predicate.java
+++ src/main/java/Predicate.java
@@ -1,8 +1,7 @@
 
 @FunctionalInterface
-public interface Predicate{
-
-
+public interface Predicate {
+    boolean test(Measurable m);
 }
 
 //This method is supposed to return something but it need one, and only one,
