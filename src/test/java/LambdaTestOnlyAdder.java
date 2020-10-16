6e851e7116a5a5d5f8baaa778fe87290a5c36fb5
ngrundback <ngrundback@gmail.com>
2020-10-16 22:21:08 +0200

    fixed file with different form of the 'adder' function

--- src/test/java/LambdaTestOnlyAdder.java
+++ src/test/java/LambdaTestOnlyAdder.java
@@ -3,10 +3,10 @@ import org.junit.jupiter.api.*; // This is Junit 5
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class LambdaTestNils {
-    
+class LambdaTestNilsOnlyAdder {
+
     @Test
-    public void testLambdas() {
+    public void testAdderLambdas() {
 
         // As Measurable is a functional interface we can instantiate Measurables through lambdas instead
         // of actually implementing Measurables in a specific subclass
@@ -21,6 +21,12 @@ class LambdaTestNils {
 
         Measurable[] array = {f1, f2, f3, f4, f5, f6, f7};
 
+// ------------------------------
+
+        // Example 1.
+        // This is how you are used to handle an interface, you create a new class that implements the
+        // interface and then that class overrides the abstract function from the interface.
+        //
         // This does NOT use anonymous classes or Lambda. This creates an explicit and named new class that implements
         // the interface BinaryFunction.
         class AdderExplicitClass implements BinaryFunction {
@@ -45,8 +51,13 @@ class LambdaTestNils {
         // Now that we have defined a new class we have to create an instance of it.
         AdderExplicitClass adderExplicitInstance = new AdderExplicitClass();
 
+        // --------
+
+        // Example 2.
+        //
         // Adder example without any lambdas, this is the function I was asking about /Anna
         // This is the same as above but as an anonymous class.
+        // See that we directly get an instance of a class without writing the word 'class' as above.
         BinaryFunction adderNoLambdaButAnonymousClass = new BinaryFunction() {
             public Measurable apply(Measurable a, Measurable b) { //
                 double sum = a.getValue() + b.getValue();
@@ -62,11 +73,15 @@ class LambdaTestNils {
             }
         };
 
+        // --------
+
+        // Example 3.
         // Adder example with one layer of lambda
+        // We replace the anonymous class with a Lambda.
         BinaryFunction adderOneLayerOfLambdas = (a, b) -> {
             double sum = a.getValue() + b.getValue();
 
-            Measurable m = new Measurable() {
+            Measurable m = new Measurable() {   // See we still use an anonymous class here!
                 @Override
                 public double getValue() {
                     return sum;
@@ -76,20 +91,26 @@ class LambdaTestNils {
             return m;
         };
 
+        // --------
+
+        // Example 4.
+        // Go one step further and replace the inner Measurable class with another Lambad.
+        // Everything is contained in one line!
         BinaryFunction adderTwoLayersOfLambdas = (a, b) -> () -> a.getValue() + b.getValue();
 
-        // Subtractor example with two layers of lambdas
+        // -----------------------------------------------------
+
+        // Now lets implement the other functions in the form of a two layer Lambda
+
         BinaryFunction subtractor = (a, b) -> () -> a.getValue() - b.getValue();
 
-
-        //BinaryFunction multiplier = null; // ToDo:  utilize method reference from MeasurableUtils
-        BinaryFunction multiplier = (a, b) -> MeasurableUtils.multiply(a,b);
-
-
-        //UnaryFunction negator = null; // ToDo: implement lambda which negates a Measurable
+        BinaryFunction multiplier = (a, b) -> MeasurableUtils.multiply(a, b);
 
         UnaryFunction negator = (a) -> () -> -a.getValue();
 
+// ------------------------------
+
+        // Now use each version of the Adder functions to see that they do the same
 
         Measurable resultExplicitAdder = negator.apply(
                 multiplier.apply(
@@ -120,82 +141,20 @@ class LambdaTestNils {
         );
 
 
+// ------------------------------
+        // And finally print the results and then check them with a Junit test
+
         // EXPECTED result after applying lambdas: -((1+1)*(2-3)) => -(2*-1) => -(-2) => 2
 
-///----------------
-        System.out.println("First test equals (explicit class) = " + resultExplicitAdder.getValue() );
-        System.out.println("First test equals (Anonymous class) = " + resultAnonymousClassAdder.getValue() );
-        System.out.println("First test equals (1 Layer of Lambda) = " + resultOneLayerOfLambdaAdder.getValue() );
-        System.out.println("First test equals (2 layers of lambdas) = " + resultTwoLayersOfLambdas.getValue() );
+        System.out.println("First test equals (explicit class) = " + resultExplicitAdder.getValue());
+        System.out.println("First test equals (Anonymous class) = " + resultAnonymousClassAdder.getValue());
+        System.out.println("First test equals (1 Layer of Lambda) = " + resultOneLayerOfLambdaAdder.getValue());
+        System.out.println("First test equals (2 layers of lambdas) = " + resultTwoLayersOfLambdas.getValue());
 
         assertEquals(2, resultExplicitAdder.getValue()); // Added test for the "explicit class"
         assertEquals(2, resultAnonymousClassAdder.getValue()); // Same for "anonymous class"
         assertEquals(2, resultOneLayerOfLambdaAdder.getValue()); // Same for "1 layer lambda"
         assertEquals(2, resultTwoLayersOfLambdas.getValue()); // Final form with two layer of lambdas
-///-----------------
-
-
-
-
-
-        //Filter filter = null;  // ToDo: implement lambda which filters the array according to a given predicate.
-        // As this will be a general-purpose method you could also put the
-        // implementation in MeasurableUtils and utilize a method reference here
-        Filter filter = (incomingArray, predicate) -> {
-            Measurable[] filteredArray = {};
-            for(Measurable currentMeasurable : incomingArray) {
-                if (predicate.test(currentMeasurable)) {
-                    Measurable[] newFilteredArray = new Measurable[filteredArray.length +1 ];
-                    System.arraycopy(filteredArray, 0, newFilteredArray, 0, filteredArray.length);
-                    newFilteredArray[filteredArray.length] = currentMeasurable;
-                    filteredArray = newFilteredArray;
-                }
-            }
-
-            return filteredArray;
-        };
-
-
-        //Measurable[] moreThan5 = filter.apply(array, null); // ToDo: implement predicate lambda which accepts
-        // Measurables with values > 5
-        Measurable[] moreThan5 = filter.apply(array, (m) -> m.getValue() > 5);
-
-
-
-
-        // EXPECTED array after filtering: [f6, f7]
-        assertArrayEquals(new Measurable[] {f6, f7}, moreThan5);
-
-        // Negate f2 and f5
-        array[1] = negator.apply(array[1]);
-        array[4] = negator.apply(array[4]);
-        //Measurable[] negativeValues = filter.apply(array, null); // ToDo: utilize method reference from MeasurableUtils
-        // to only keep the negative values
-        Measurable[] negativeValues = filter.apply(array, (m) -> MeasurableUtils.isNegative(m));
-
-        // EXPECTED array after filtering: [-f2, -f5]
-        assertEquals(2, negativeValues.length);
-        assertArrayEquals(new double[] {-f2.getValue(),-f5.getValue()},
-                new double[] { negativeValues[0].getValue(),
-                        negativeValues[1].getValue()});
-
-        //Reducer addition = null; // ToDo: Implement lambda for adding all values together
-        Reducer addition = (arrayToSum) -> {
-            double sumDouble = 0;
-            for (Measurable currentMeasurable : arrayToSum) {
-                sumDouble += currentMeasurable.getValue();
-            }
-
-            double finalSumDouble = sumDouble; // We are not allowed to reference something we have modified. So create a copy that is final...
-            Measurable sumMeasurable = () -> finalSumDouble;
-
-            return sumMeasurable;
-        };
-
-
-        Measurable sumOfAllValues = addition.reduce(array);
-
-        // EXPECTED sum: 1 -1 + 2 + 3 - 5 + 8 + 13 = 21
-        assertEquals(21, sumOfAllValues.getValue());
+// ------------------------------
     }
 }
